6b8ff054ab544b122ade8694583fb2fe77c9c067
Zoran Pandovski <zoran.pandovski@keitaro.com>
2019-08-27 14:50:13 +0200

    Add site url

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Zoran Pandovski'
 SITENAME = 'Quant Server Documentation'
-SITEURL = ''
+SITEURL = 'https://tmqr.github.io/quant-server-docs/'
 
 PATH = 'content'
 
